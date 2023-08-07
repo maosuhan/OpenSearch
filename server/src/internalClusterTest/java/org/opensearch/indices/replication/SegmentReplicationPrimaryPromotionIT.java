bdb6250f742bff7d77531f365e1f025fbc02974b
Suhan Mao <maosuhan@bytedance.com>
2023-08-07 21:02:51 +0800

    adjust SegmentReplicationPrimaryPromotionIT

--- server/src/internalClusterTest/java/org/opensearch/indices/replication/SegmentReplicationPrimaryPromotionIT.java
+++ server/src/internalClusterTest/java/org/opensearch/indices/replication/SegmentReplicationPrimaryPromotionIT.java
@@ -27,33 +27,25 @@
 package org.opensearch.indices.replication;
 
 import org.opensearch.action.ActionFuture;
-import org.opensearch.action.admin.indices.close.CloseIndexResponse;
 import org.opensearch.action.index.IndexRequest;
 import org.opensearch.action.index.IndexResponse;
 import org.opensearch.cluster.ClusterState;
 import org.opensearch.cluster.metadata.IndexMetadata;
 import org.opensearch.cluster.node.DiscoveryNode;
-import org.opensearch.cluster.routing.IndexShardRoutingTable;
 import org.opensearch.cluster.routing.ShardRouting;
 import org.opensearch.common.settings.Settings;
 import org.opensearch.indices.replication.common.ReplicationType;
-import org.opensearch.plugins.Plugin;
 import org.opensearch.test.InternalTestCluster;
 import org.opensearch.test.OpenSearchIntegTestCase;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertAcked;
 import static org.opensearch.test.hamcrest.OpenSearchAssertions.assertHitCount;
 
 @OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.TEST, numDataNodes = 2)
-public class SegmentReplicationPrimaryPromotionIT extends OpenSearchIntegTestCase {
+public class SegmentReplicationPrimaryPromotionIT extends SegmentReplicationBaseIT {
 
     @Override
     public boolean addMockInternalEngine() {
@@ -61,47 +53,43 @@ public class SegmentReplicationPrimaryPromotionIT extends OpenSearchIntegTestCas
     }
 
     public void testPromoteReplicaToPrimary() throws Exception {
-        final String indexName = randomAlphaOfLength(5).toLowerCase(Locale.ROOT);
+        String[] dataNodeNames = getClusterState().nodes().getDataNodes().values().stream().map(DiscoveryNode::getName).toArray(String[]::new);
+        assertEquals(2, dataNodeNames.length);
+        final String indexName = INDEX_NAME;
         createIndexInternal(indexName);
         ensureGreen();
 
         final int numOfDocs = 3;
-        List<IndexRequest> requestList = new ArrayList<>();
+        List<ActionFuture<IndexResponse>> responses = new ArrayList<>();
         for (int i = 0; i < numOfDocs; i++) {
-            requestList.add(new IndexRequest(indexName).source("long_fields", 1));
+            responses.add(client().index(new IndexRequest(indexName).source("long_fields", 1)));
         }
-        List<ActionFuture<IndexResponse>> responseList = new ArrayList<>();
-        requestList.forEach(request -> responseList.add(client().index(request)));
-        responseList.forEach(ActionFuture::actionGet);
-
-        refresh(indexName);
-        // primary and replica refresh is not consistent currently in segment replication mode
-        Thread.sleep(1000L);
-        assertHitCount(client().prepareSearch(indexName).setSize(0).get(), numOfDocs);
-        ensureGreen(indexName);
+        assertBusy(() -> {
+                responses.forEach(ActionFuture::actionGet);
+                refresh(indexName);
+            }
+        );
+        waitForSearchableDocs(numOfDocs, dataNodeNames);
 
         // pick up a data node that contains a random primary shard
         ClusterState state = client(internalCluster().getMasterName()).admin().cluster().prepareState().get().getState();
-        final int numShards = state.metadata().index(indexName).getNumberOfShards();
-        final ShardRouting primaryShard = state.routingTable().index(indexName).shard(randomIntBetween(0, numShards - 1)).primaryShard();
-        final DiscoveryNode randomNode = state.nodes().resolveNode(primaryShard.currentNodeId());
+        final ShardRouting primaryShard = state.routingTable().index(indexName).shard(0).primaryShard();
+        final ShardRouting replicaShard = state.routingTable().index(indexName).shard(0).replicaShards().get(0);
+        final DiscoveryNode primaryNode = state.nodes().resolveNode(primaryShard.currentNodeId());
+        final DiscoveryNode replicaNode = state.nodes().resolveNode(replicaShard.currentNodeId());
 
-        // stop the data node of primary shard, all remaining shards are promoted to primaries
-        internalCluster().stopRandomNode(InternalTestCluster.nameFilter(randomNode.getName()));
+        internalCluster().stopRandomNode(InternalTestCluster.nameFilter(primaryNode.getName()));
         ensureYellowAndNoInitializingShards(indexName);
+        final DiscoveryNode newPrimaryNode = replicaNode;
 
         state = client(internalCluster().getMasterName()).admin().cluster().prepareState().get().getState();
-        for (IndexShardRoutingTable shardRoutingTable : state.routingTable().index(indexName)) {
-            for (ShardRouting shardRouting : shardRoutingTable.activeShards()) {
-                assertThat(shardRouting + " should be promoted as a primary", shardRouting.primary(), is(true));
-            }
-        }
-
+        ShardRouting newPrimaryShardRouting = state.routingTable().index(indexName).shards().get(0).primaryShard();
+        assertEquals(newPrimaryNode.getId(), newPrimaryShardRouting.currentNodeId());
         assertHitCount(client().prepareSearch(indexName).setSize(0).get(), numOfDocs);
     }
 
     private void createIndexInternal(String index) {
-        client().admin()
+        assertAcked(client().admin()
             .indices()
             .prepareCreate(index)
             .setMapping("{\"_doc\":{\"properties\":{\"long_field\":{\"type\":\"long\"}}}}")
@@ -110,7 +98,6 @@ public class SegmentReplicationPrimaryPromotionIT extends OpenSearchIntegTestCas
                     .put(IndexMetadata.SETTING_NUMBER_OF_REPLICAS, 1)
                     .put(IndexMetadata.SETTING_NUMBER_OF_SHARDS, 1)
                     .put(IndexMetadata.INDEX_REPLICATION_TYPE_SETTING.getKey(), ReplicationType.SEGMENT)
-            )
-            .get();
+            ));
     }
 }
